579829f5620eaebf166fb40701a44968b4ea68c4
bhaskarreddy <bhaskrrd10@gmail.com>
2022-06-08 10:16:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+
+response = requests.get(URL)
+website_html = response.text
+
+soup= BeautifulSoup(website_html, "html.parser")
+
+print(soup.prettify())
