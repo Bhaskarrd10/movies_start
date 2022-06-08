4a547da35f9beecae564e125c549aafd56f4d146
bhaskarreddy <bhaskrrd10@gmail.com>
2022-06-08 10:29:21 +0530

    writing file with 100 movie list

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ website_html = response.text
 
 soup= BeautifulSoup(website_html, "html.parser")
 
-print(soup.prettify())
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+#print(movies)
+
+with open("movies.txt", mode ="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
